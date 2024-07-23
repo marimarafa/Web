8c6cc85f920682ee93c5bc8af437da57df7439f4
marimarafa <arafamarim166@gmail.com>
2024-07-23 12:25:46 +0200

    ..

---
+++ Web1/Lezione4/Server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+app.use(cors());
+var iPortaTcp = 4201;
+var sIpAddress = "127.0.0.1"
+app.listen(iPortaTcp,sIpAddress, () => console.log('API is running on http://' + sIpAddress +
+':' + iPortaTcp));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//pagina di invio della form
+app.get('/', (req, res) => {
+    console.log("Mi hai chiesto la form di registrazione");
+    res.sendFile("formSemplice.html", { root: './htdoc' });
+    });
+
+//pagina di gestione dei dati della form se il metodo è GET
+app.get('/gestisciDatiForm', (req, res) => {
+    console.log(req.query.fname);
+    console.log(req.body.fcognome);
+    res.send("<html>Buona serata "+ req.query.fname +" "+ req.query.fcognome + "</html>");
+    })
